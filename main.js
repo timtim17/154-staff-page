575b7e358b3d11a5d4a6f3fe7ca455cd8435d2aa
Austin Jenchi <ajenchi9@gmail.com>
2019-06-19 01:20:33 -0700

    start easter egg by tracking key presses

--- main.js
+++ main.js
@@ -13,6 +13,16 @@
 
     const ANIM_TIME_SLIDE = 1000;
 
+    const KEY_ARROW_UP = "ARROWUP";
+    const KEY_ARROW_DOWN = "ARROWDOWN";
+    const KEY_ARROW_LEFT = "ARROWLEFT";
+    const KEY_ARROW_RIGHT = "ARROWRIGHT";
+    const KEY_ENTER = "ENTER";
+    const EGG_KEY_COMBO = [KEY_ARROW_UP, KEY_ARROW_UP, KEY_ARROW_DOWN, KEY_ARROW_DOWN,
+        KEY_ARROW_LEFT, KEY_ARROW_RIGHT, KEY_ARROW_LEFT, KEY_ARROW_RIGHT, "B", "A", KEY_ENTER];
+    const eggKeysTyped = [];
+    let eggFound = false;
+
     window.addEventListener("load", init);
 
     /**
@@ -21,6 +31,7 @@
     function init() {
         id("btn-left").addEventListener("click", () => changeSlide(-1));
         id("btn-right").addEventListener("click", () => changeSlide()); // default param
+        document.body.addEventListener("keydown", easterEggTyped);
     }
 
     /**
@@ -71,6 +82,48 @@
             slide.classList.toggle(CLASS_HIDDEN);
             slide.classList.remove(CLASS_SLIDE_OFF);
         }, ANIM_TIME_SLIDE);
+    }
+
+    /**
+     * Handles a key press to check if it matches the trigger for the easter egg. If the combo has
+     * been entered, start the easter egg.
+     *
+     * @param {KeyboardEvent} event - The event from the keydown event of the button being pressed
+     */
+    function easterEggTyped(event) {
+        if (!eggFound) {
+            eggKeysTyped.push(event.key.toUpperCase());
+            while (eggKeysTyped.length > EGG_KEY_COMBO.length) {
+                eggKeysTyped.shift();   // drop elements from the beginning,
+                                        // since keys are added to the end
+            }
+            if (twoArraysEqual(eggKeysTyped, EGG_KEY_COMBO)) {
+                console.log("easter egg!");
+                eggFound = true;
+            }
+        }
+    }
+
+    /**
+     * Checks if the two arrays have the same items. Neither array is modified.
+     * If they are not the same length they are assumed to be different. The items
+     * are considered equal if they have the same value and type (check by `===`).
+     *
+     * @param {any[]} arrayOne - The first array
+     * @param {any[]} arrayTwo - The second array
+     * @return {boolean} true if they have the same items, false otherwise
+     */
+    function twoArraysEqual(arrayOne, arrayTwo) {
+        if (arrayOne.length === arrayTwo.length) {
+            for (let i = 0; i < arrayOne.length; i++) {
+                if (arrayOne[i] !== arrayTwo[i]) {
+                    return false;
+                }
+            }
+            return true;
+        } else {
+            return false;
+        }
     }
 
     /* CSE 154 HELPER FUNCTIONS */
